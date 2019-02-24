a62e2fbbc072157088d152c5d396b421fc6647fd
sickfriends <47953594+sickfriends@users.noreply.github.com>
2019-02-24 14:12:34 -0800

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
 var left = document.getElementById('l');
 var right = document.getElementById('r');
 var title = document.getElementById('title');
