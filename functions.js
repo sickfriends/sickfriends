f1d1a5f3930bc31cdf3736734cfcc2075d6c501c
sickfriends <47953594+sickfriends@users.noreply.github.com>
2019-02-24 13:56:00 -0800

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,32 @@
+var left = document.getElementById('l');
+var right = document.getElementById('r');
+var title = document.getElementById('title');
+
+var onScroll = (function(){
+  var startPos;
+  function run(){
+    var fromTop = window.pageYOffset, 
+        rect = left.getBoundingClientRect(),
+        scrollDelta;
+    // check if element is in viewport
+    if( (rect.top - window.innerHeight) <= 0 && rect.bottom > 0 )
+       startPos = startPos === undefined ? fromTop : startPos;
+    else{
+      startPos = 0;
+      return;
+    }   
+    scrollDeltaL = (fromTop - startPos) * -0.25; // left logo speed
+    scrollDeltaR = (fromTop - startPos) * 0.25; // right logo speed
+    scrollDeltaTop = (fromTop - startPos) * -0.35; // header/title speed moving up
+    left.style.transform = `translateX(${scrollDeltaL}px)`;
+    right.style.transform = `translateX(${scrollDeltaR}px)`;
+    title.style.transform = `translateY(${scrollDeltaTop}px) rotate(${scrollDeltaTop}deg)`;
+    console.clear();
+    console.log(scrollDelta);
+  }
+  run();
+  return run;
+})()
+
+
+window.addEventListener('scroll', onScroll);
